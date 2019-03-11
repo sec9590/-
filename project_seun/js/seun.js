4d17aba3aca746d9d9f10e38fbdf272ed2eb53de
sec590 <sec9590@gmail.com>
2019-03-11 20:12:59 +0900

    modify 20190311

--- project_seun/js/seun.js
+++ project_seun/js/seun.js
@@ -37,3 +37,26 @@ function controlplay() { //배경음악 컨트롤
   }
 
 }
+
+$(document).ready(function(){
+  $('#red1').css('opacity', 0);
+
+  $('#red1').waypoint(function() {
+      $('#red1').addClass('slideInUp');
+  }, { offset: '50%' });
+
+});
+
+$(document).ready(function() {
+    $(window).scroll( function(){
+        $('.fadeinleft').each( function(i){
+            var bottom_of_element = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+            if( bottom_of_window > bottom_of_element-250 ){
+                $(this).animate({'opacity':'1','margin-left':'0px'},1000);
+            }
+
+        });
+    });
+});
